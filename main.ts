12fea7d1b50d32117827bac7999f5f21b18bbf42
sebastianzcoder <70339721+sebastianzcoder@users.noreply.github.com>
2023-11-11 22:02:16 +0200

    added tail to the meteor ☄

--- main.ts
+++ main.ts
@@ -5,8 +5,10 @@ input.onButtonPressed(Button.B, function () {
     rocketship.change(LedSpriteProperty.X, 1)
 })
 let rocketship: game.LedSprite = null
+let tail = game.createSprite(2, 0)
 rocketship = game.createSprite(2, 4)
 let meteor = game.createSprite(2, 0)
+tail.set(LedSpriteProperty.Brightness, 100)
 basic.forever(function () {
     meteor.set(LedSpriteProperty.X, randint(0, 4))
     basic.pause(200)
@@ -32,4 +34,8 @@ basic.forever(function () {
         }
     }
     meteor.set(LedSpriteProperty.Y, 0)
+})
+basic.forever(function () {
+    tail.set(LedSpriteProperty.X, meteor.get(LedSpriteProperty.X))
+    tail.set(LedSpriteProperty.Y, meteor.get(LedSpriteProperty.Y) - 1)
 })
